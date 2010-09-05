8ae46d50e5b5385bdb60e177cc473e4982726a1a
Nicholas Hibberd <Nick@Nicholas-Hibberds-MacBook-Pro.local>
2010-09-05 18:11:15 +0100

    removing mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,6 @@ class UsersController < AdminController
       else
         render 'admin/users/new' 
       end
-      
-      respond_to do |format|
-            if @user.save
-              # Tell the UserMailer to send a welcome Email after save
-              UserMailer.welcome_email(@user).deliver
-
-              format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-              format.xml  { render :xml => @user, :status => :created, :location => @user }
-            else
-              format.html { render :action => "new" }
-              format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-            end
-          end
   end 
   
    def update
--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,10 +1,3 @@
 class UserMailer < ActionMailer::Base
-  default :from => "nicholashibberd@gmail.com"
-  
-  def welcome_email(user)
-    @user = user
-    @url = "http://localhost:3000/admin"
-    mail(:to => "nicholashibberd@hotmail.com", :subject => "Welcome to my site")
-  end
 
 end
